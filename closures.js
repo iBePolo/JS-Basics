24d7ff62cd112b8e4b9e22440d48982cdab79f72
Marco L <ibepolo@gmail.com>
2015-08-30 15:34:24 -0600

    updates

--- closures.js
+++ closures.js
@@ -11,9 +11,14 @@ var outer = function(){
 
   //Code Here
 
-//Once you do that, invoke inner.
+var inner = outer();
 
+//Once you do that, invoke inner.
   //Code Here
+
+  inner();
+
+
 
 
 
@@ -34,7 +39,9 @@ var callFriend = function(){
 
   //Code Here
 
+var calling = callFriend();
 
+calling("435-215-9248");
 
 //Next Problem
 
@@ -45,6 +52,15 @@ var callFriend = function(){
 */
 
   //Code Here
+
+  function makeCounter() {
+    var counter = 0;
+    return function () {
+      counter++;
+      return counter;
+    }
+  }
+
   var count = makeCounter();
   count() // 1
   count() // 2
@@ -62,7 +78,109 @@ var callFriend = function(){
   (which invokes the original function that was passed in) that can only ever be executed once.
 */
 
+function outerFunction(anyFunction) {
+  var hasBeenRun = false;
+  function inner() {
+    if (hasBeenRun) {
+    return "STOP";
+  } else {
+    hasBeenRun = true;
+    return anyFunction();
+    }
+
+  }
+  return inner;
+}
+
+function myNameFunc() {
+  return "Marco!"
+}
+
+var callingOuterFunction = outerFunction(myNameFunc);
+
+console.log(callingOuterFunction());
+console.log(callingOuterFunction());
+console.log(callingOuterFunction());
+
+
+
+// ** Mark's answer
+  // var someFunction = function(anotherFunction){
+  //   var count = 0
+  //   var newFunction = function(){
+  //     if (count == 0){
+  //       count++
+  //       return anotherFunction();
+  //     } else {
+  //       return 'you have already invoked this function'
+  //     }
+  //
+  //   }
+  //   return newFunction;
+  // }
+  //
+  // var sayHi = function(){
+  //   console.log('hi')
+  // }
+
   //Code Here
+
+  // *** Failed attempts***
+//   function myNameFunc() {
+//     return "My name is Slim Shady!!!"
+//   }
+//
+//   myNameFunc();
+//
+// function funcCaller(yourFunc) {
+//   return function () {
+//     yourFunc;
+//   }
+//   // return a;
+// }
+// funcCaller(myNameFunc);
+// funcCaller(myNameFunc());
+//
+// var c = funcCaller(myNameFunc);
+// var d = funcCaller(myNameFunc());
+// var e = funcCaller();
+// var f = funcCaller;
+//
+// c;
+// d;
+// c();
+// d();
+//
+//
+//
+//
+// var myFunction = function () {
+//   return "hi";
+// }
+//
+//
+// var outerFunction = function (yourFunction) {
+//   var counter = 0;
+//   var functionInner = function () {
+//     if (counter === 0) {
+//       counter++;
+//       return yourFunction;
+//     } else {
+//       return "Already done";
+//     }
+//   }
+//   return functionInner;
+// }
+//
+//
+// var functionInvoke = outerFunction();
+// functionInvoke(myFunction);
+//
+//
+//
+//
+
+
 
 
 
@@ -73,6 +191,62 @@ var callFriend = function(){
 /*
   Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. The first parameter will be an anonymous function and the second parameter, 'N', will be a number. Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. After it's been invoked 'N' number of times, return 'STOP'.
 */
+/*
+function fnCounter(aFunction, n) {
+  var counter = 0;
+        console.log(counter);
+  if (counter < n) {
+     function count () {
+      counter++;
+      console.log(counter);
+      return aFunction();
+      console.log(counter);
+    }
+    } else {
+      return "STOP";
+    }
+    return count;
+  }///*/
+
+
+
+function fnCounter(aFunction, n) {
+  var counter = 0;
+        console.log(counter);
+  function count () {
+    if (counter < n) {
+      counter++;
+      console.log(counter);
+      return aFunction();
+    } else {
+      return "STOP";
+    }
+  }
+  return count;
+}
+
+
+var myFunction = function () {
+  return "hi";
+}
+
+var runFnCounter = fnCounter(myFunction, 3);
+
+console.log(runFnCounter());
+
+
+// function fnCounter(aFunction, n) {
+//   var counter = 0;
+//   function count () {
+//     for (var i = 0; i < counter; i++) {
+//       return aFunction();
+//     } if (i >= n) {
+//       return "STOP";
+//     }
+//   }
+//   return count;
+// }
+
 
 
 
@@ -93,13 +267,15 @@ var callFriend = function(){
 
     //Answer Here
 
+    // I believe that it will be called at 1 millisecond
+
 
   Now, run the function in your console and note what happpens.
 
   Was your answer right or wrong?
 
     //Answer Here
-
+    // I was wrong, it didn't run at all.
 
   Fix the counter function so that it works the way you expect it to work. (logging 1 then 2 then 3, etc)
 */
@@ -125,4 +301,18 @@ var callFriend = function(){
   *Hint: Don't let this fool you. Break down what's really happening here.
 */
 
+/*
+  What is happening is: An array is holding functions.
+*/
+var myArr = [function() { return "hi";}];
 
+var funcArray = [
+  function() {return 0;},
+  function() {return 1;},
+  function() {return 2;},
+  function() {return 3;},
+  function() {return 4;},
+  function() {return 5;}
+];
+
+funcArray[0]();
--- fns-and-scope.js
+++ fns-and-scope.js
@@ -7,6 +7,30 @@ var name = 'Tyler';
 
   //Code Here
 
+var tylerFunction = isTyler(name) {
+  if (name === "Tyler") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// or
+
+function isTyler(name) {
+  if (name === "Tyler" || name === "tyler") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// or
+
+
+function isTyler(name) {
+  return (name === "Tyler" || name === "tyler");
+}
 
 //Next problem
 
@@ -16,6 +40,23 @@ var name = 'Tyler';
 
 
   //Code Here
+function getName() {
+  var userName = prompt("What's your name?");
+  return userName;
+}
+ var yourName = getName();
+ alert(yourName);
+
+// or
+
+function getName() {
+  return prompt("What's your name?");
+  // return userName;
+}
+ var yourName = getName();
+ alert(yourName);
+
+
 
 
 //Next Problem
@@ -26,7 +67,23 @@ var name = 'Tyler';
 //then alerts "Welcome, " plus whatever the users name is.
 
   //Code Here
+function welcome() {
+  var a = getName();
+  return alert("Welcome " + getName);
+}
 
+var hi = welcome();
+hi();
+
+// correct answer:
+
+function welcome() {
+  var name = getName();
+
+  alert("Welcome " + name);
+}
+
+welcome();
 
 //Next problem
 
@@ -36,6 +93,9 @@ var name = 'Tyler';
 //What is the difference between arguments and parameters?
 
   //Answer Here
+
+  // Parameters: The place holders/variable used to declare the function
+  // Arguments: What is passed through the function
 
 
 //Next problem
@@ -57,11 +117,36 @@ var name = 'Tyler';
 
   //Code Here
 
+function myName() {
+  return "Marco";
+}
+
 //Now save the function definition of myName into a new variable called newMyName
 
   //Code Here
+var user = myName;
 
 //Now alert the result of invoking newMyName
+
+alert(user());
+
+// another way of my own - Teacher's(Jared Stark) answer below *********************
+
+function myName(a) {
+  return a;
+}
+//Now save the function definition of myName into a new variable called newMyName
+  //Code Here
+var user = myName;
+//Now alert the result of invoking newMyName
+alert(user("Marco"));
+
+// Teacher (Jared Stark) answer
+
+// see my 1st version for correct answer
+
+
+
 
 
 
@@ -72,9 +157,54 @@ var name = 'Tyler';
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
+function outerFn() {
+  return function() {
+    return "Marco";
+  }
+}
 
 //Now save the result of invoking outerFn into a variable called innerFn.
-
   //Code Here
 
+  var innerFn = outerFn();
+
 //Now invoke innerFn.
+
+innerFn();
+
+// Teacher(Jared Stark) answer
+
+function outer () {
+  return function () {
+    return "Marco";
+  }
+}
+
+var inner = outer(); // inner value = the annonymous function inside the "outer" function
+
+// Below: Playing around.
+/*
+function outer () {
+  return function a (x) {
+    return a( alert(x));
+  }
+}
+
+var inner = outer(4);
+*/
+
+
+
+// Closure example:
+function welcomer(greeting) {
+  return function (name) {
+    alert(greeting + " " + name);
+  }
+}
+
+var englishWelcomer = welcomer("Welcome");
+var spanishWelcomer = welcomer("Bienvenido");
+
+englishWelcomer("Jared");
+englishWelcomer("Frank");
+spanishWelcomer("Isaac");
